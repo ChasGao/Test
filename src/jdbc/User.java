70e8b207e0a5d036f38e725f5cc1d99583036671
chas <3086816120@qq.com>
2017-07-14 11:37:24 +0800

    Create User.java

---
+++ src/jdbc/User.java
@@ -0,0 +1,28 @@
+package jdbc;
+
+public class User {
+
+	private Integer id;
+	private String loginName;
+	private String name;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getLoginName() {
+		return loginName;
+	}
+	public void setLoginName(String loginName) {
+		this.loginName = loginName;
+	}
+	
+}
